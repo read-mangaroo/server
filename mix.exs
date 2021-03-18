a32e32f67704f13ac7801d4c81248a183fc6bfe7
Terence Ponce <terenceponce@gmail.com>
2021-03-18 23:00:25 +0800

    Add Credo for linting
    This adds Credo as our linter.

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Mangaroo.MixProject do
       {:telemetry_poller, "0.5.1"},
       {:gettext, "0.18.2"},
       {:jason, "1.2.2"},
-      {:plug_cowboy, "2.4.1"}
+      {:plug_cowboy, "2.4.1"},
+      {:credo, "1.5.5", only: [:dev, :test], runtime: false}
     ]
   end
 
@@ -57,7 +58,8 @@ defmodule Mangaroo.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      lint: ["format", "credo"]
     ]
   end
 end
